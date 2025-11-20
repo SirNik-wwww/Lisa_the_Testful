f632bab110bde3305e477ef5c47c73de47730ac5
SirNik-wwww <nikitcreator@gmail.com>
2025-11-20 12:50:11 +1000

    2025_11_20_1_lisa

--- bread_v3.gd
+++ bread_v3.gd
@@ -4,20 +4,20 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 12.5
 var dir_is_foward : bool = true
+var may_move : bool = false
+var time_to_move_l_o_r := 0.5
+var action_released : bool = true
+
+@onready var m_holder: Node3D = $MarkHolder
+@onready var m_right: Marker3D = $MarkHolder/MarkerRight
+@onready var m_left: Marker3D = $MarkHolder/MarkerLeft
+
+
 
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta * 4
-
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
 
@@ -28,16 +28,23 @@ func _input(_event: InputEvent) -> void:
 		if $ShapeCast3D.is_colliding() == true:
 			velocity.y += JUMP_VELOCITY
 
+# Код из холи реал
+		#var terminal_back = Vector3(backdoor.position.x, backdoor.position.y +4, backdoor.position.z)
+		#get_tree().create_tween().tween_property(backdoor, "position", terminal_back, 0.4)
+		#var terminal_1 = Vector3(static_body_3d.position.x, static_body_3d.position.y +5.1, static_body_3d.position.z)
+		#get_tree().create_tween().tween_property(static_body_3d, "position", terminal_1, 0.4)
+
 
 	if Input.is_action_pressed("ui_left"):
-		anim_pl.play("left")
+		move_left_right("left")
 	if Input.is_action_just_released("ui_left") and !Input.is_action_pressed("ui_right"):
-		anim_pl.play("left_s")
+		action_released = true
+		#anim_pl.play("left_s")
 
 	if Input.is_action_pressed("ui_right"):
-		anim_pl.play("right")
+		move_left_right("right")
 	if Input.is_action_just_released("ui_right") and !Input.is_action_pressed("ui_left"):
-		anim_pl.play("right_s")
+		action_released = true
 
 	if Input.is_action_pressed("ui_left") != true and Input.is_action_pressed("ui_right") != true:
 		if Input.is_action_pressed("ui_up"):
@@ -45,3 +52,25 @@ func _input(_event: InputEvent) -> void:
 			
 		if Input.is_action_pressed("ui_down"):
 			anim_pl.play("foward")
+
+
+	if Input.is_action_pressed("ui_cancel"):
+		get_tree().reload_current_scene()
+
+
+func move_left_right(direction : String):
+	if may_move == false:
+		may_move = !may_move
+		var next_pos_left := m_left.global_position
+		var next_pos_rigth := m_right.global_position
+		if direction == "left":
+			get_tree().create_tween().tween_property(self, "position", next_pos_left, time_to_move_l_o_r)
+		if direction == "right":
+			get_tree().create_tween().tween_property(self, "position", next_pos_rigth, time_to_move_l_o_r)
+		anim_pl.play(direction)
+		await get_tree().create_timer(time_to_move_l_o_r).timeout
+		if action_released == true:
+			var dir_name = direction + "_s"
+			anim_pl.play(dir_name)
+		may_move = !may_move
+		
